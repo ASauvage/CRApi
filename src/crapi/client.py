6306b549b94043c092e479b4540c0647209507f1
Alexandre Sauvage Potel <a.sauvagepotel@gmail.com>
2026-01-12 21:36:55 +0100

    [feature] update client

--- src/crapi/client.py
+++ src/crapi/client.py
@@ -1,13 +1,13 @@
 from .http.client import HttpClient
-from .resources import ClansRessources, PlayersRessource
+from .resources import ClansResource, PlayersRessource
 
 
 class CRApiClient:
     def __init__(self, token: str | None = None, version: str = "v1") -> None:
         http = HttpClient(
-            base_url="",
+            hostname="https://api.clashroyale.com/",
+            version=version,
             token=token
         )
 
-        self.clans = ClansRessources(http)
-        self.players = PlayersRessource(http)
+        self.clans = ClansResource(http)
